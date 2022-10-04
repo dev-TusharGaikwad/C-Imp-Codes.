f87a1b2066432676efa62acf23472edfe8cbea03
devSython2 <92568243+devSython2@users.noreply.github.com>
2022-10-04 20:21:21 +0530

    Update Greatest common divisor.c

--- Greatest common divisor.c
+++ Greatest common divisor.c
@@ -7,19 +7,19 @@ int main()
 {
 	int gcd, remainder, numerator, denominator;
  
-    printf("Enter the numerator and denominator respectively numbers\n");
-    scanf("%d %d", &numerator, &denominator);
-    
-    remainder = numerator % denominator;
-    while (remainder != 0)
-    {
-    	numerator = denominator;
-        denominator = remainder;
-        remainder = numerator % denominator;
-    }
-    
-    gcd = denominator;
-    printf("GCD of %d and %d = %d\n", input_1, input_2, gcd);
-	
-    return 0;
+	printf("Enter the numerator and denominator respectively numbers\n");
+	scanf("%d %d", &numerator, &denominator);
+
+	remainder = numerator % denominator;
+	while (remainder != 0)
+	{
+	numerator = denominator;
+	denominator = remainder;
+	remainder = numerator % denominator;
+	}
+
+	gcd = denominator;
+	printf("GCD of %d and %d = %d \n", input_1, input_2, gcd);
+
+	return 0;
 }
