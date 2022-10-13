cf945d347cad80e50c6bf81426be7dcdfa84d9da
devSython7 <93436638+devSython7@users.noreply.github.com>
2022-10-13 20:50:02 +0530

    Update leap year.c

--- leap year.c
+++ leap year.c
@@ -4,11 +4,12 @@
 
 int main()
 {
+	// simple program to check whether the entered year is leap year or not
 	int user_input;
 	printf("Enter Any Year : ");
 	scanf("%d",&user_input);
 	
-	if(user_input%4 == 0)
+	if(user_input%4 == 0 && user_input%400 == 0)
 	{
 		printf("\nEntered Year is Leap Year");
 	}
